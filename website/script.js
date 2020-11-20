d3cf9289e30694a6ebc212b1cb18390dfd86511d
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2020-11-20 13:26:55 +0100

    revert to 65265ba01d

--- website/script.js
+++ website/script.js
@@ -1,32 +1,33 @@
-// todo: add relative coordinates
 var positions = [
-    {top:"20", right:"14"},//blue right up
-    {top:"60", right:"25"},//orange left middle_down
-    {top:"33", right:"27"},//orange_red left middle_up
-    {top:"46", right:"28"},//pink left middle
-    {top:"55", right:"7"},//purple right middle_down
+    {top:"21", right:"18"},//blue right up 01
+    {top:"60", right:"25"},//orange left middle_down 02
+    {top:"33", right:"23"},//orange_red left middle_up 03
+    {top:"46", right:"27"},//pink left middle 04
+    {top:"55", right:"8"},//purple right middle_down 05
+    {top:"40", right:"16"},//red midle_right midle 06
+    {top:"38", right:"19"},//white midle midle 07
+    {top:"30", right:"14"},//pink2 right up 08
+    {top:"54", right:"17"},//yellow midle midle_down 09
+    {top:"50", right:"11"},//blue right midle 10
+    {top:"65", right:"9"},//orange right down 11
+    {top:"70", right:"19"},//orange_red midle down 12
+    {top:"72", right:"30"},//pink left down 13
+    {top:"51", right:"22"},//purple left midle 14
 ];
 
 var ballResourcePath = "img/balls/";
 
 var ballImages = [
-    "blue_ball.png",
-    "orange_ball.png",
-    "orange_red_ball.png",
-    "pink_ball.png",
-    "purple_ball.png",
-    "red_ball.png",
-    "white_ball.png",
-    "yellow_ball.png"
+    "blue_ball.png",//0
+    "orange_ball.png",//1
+    "orange_red_ball.png",//2
+    "pink_ball.png",//3
+    "purple_ball.png",//4
+    "red_ball.png",//5
+    "white_ball.png",//6
+    "pink_ball.png",//7
+    "yellow_ball.png"//8
 ];
-
-function on_click() {
-    alert("ball pressed")
-};
-
-function randomInt(bound) {
-    return Math.floor(Math.random() * bound);
-}
 
 // https://stackoverflow.com/questions/5968196/how-do-i-check-if-a-cookie-exists
 function getCookie(name) {
@@ -54,6 +55,14 @@ function getCookie(name) {
     return decodeURI(dc.substring(begin + prefix.length, end) ).replace(/\"/g, ''); 
 }
 
+function on_click() {
+    alert("ball pressed")
+};
+
+function randomInt(bound) {
+    return Math.floor(Math.random() * bound);
+}
+
 function on_load() {
     var ballContainer = document.getElementById("treecontainer");
     var ballImageIndexes = [];
@@ -63,7 +72,7 @@ function on_load() {
         console.log(getCookie("balls"))
         ballImageIndexes = JSON.parse(getCookie("balls"));
     }
-    
+
     for (var i = 0; i < positions.length; i++) {
         var currentPosition = positions[i];
         var currentBall = document.createElement("img");
@@ -77,7 +86,7 @@ function on_load() {
             ballImage = ballImageIndexes[i];
         }
         currentBall.src = ballResourcePath + ballImages[ballImage];
-        
+
         currentBall.style.top = currentPosition.top + "%";
         currentBall.style.right = currentPosition.right + "%";
         currentBall.style.width = "40px";
