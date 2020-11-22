545a107989847783b157f89a438aa39764f29082
Streamer272 <70911709+Streamer272@users.noreply.github.com>
2020-11-22 10:14:41 +0100

    updates
    Added login describtion to css, working on unavailable symbols in js

--- website/script.js
+++ website/script.js
@@ -1,20 +1,21 @@
 var backendURL = "http://92.52.4.175:8080/";
 
 var positions = [
-    {top:"20", right:"48"},//blue right up 01
-    {top:"60", right:"45"},//orange left middle_down 02
-    {top:"33", right:"43"},//orange_red left middle_up 03
-    {top:"46", right:"47"},//pink left middle 04
-    {top:"55", right:"18"},//purple right middle_down 05
-    {top:"40", right:"26"},//red midle_right midle 06
-    {top:"38", right:"59"},//white midle midle 07
-    {top:"30", right:"34"},//pink2 right up 08
-    {top:"54", right:"57"},//yellow midle midle_down 09
-    {top:"50", right:"71"},//blue right midle 10
-    {top:"65", right:"29"},//orange right down 11
-    {top:"70", right:"39"},//orange_red midle down 12
-    {top:"72", right:"60"},//pink left down 13
-    {top:"51", right:"32"},//purple left midle 14
+    {top:"20", right:"48"},//right up 01
+    {top:"60", right:"45"},//left middle_down 02
+    {top:"33", right:"43"},//left middle_up 03
+    {top:"46", right:"47"},//left middle 04
+    {top:"55", right:"18"},//right middle_down 05
+    {top:"40", right:"26"},//midle_right midle 06
+    {top:"38", right:"59"},//midle midle 07
+    {top:"30", right:"34"},//right up 08
+    {top:"54", right:"57"},//midle midle_down 09
+    {top:"50", right:"71"},//right midle 10
+    {top:"65", right:"29"},//right down 11
+    {top:"70", right:"39"},//midle down 12
+    {top:"72", right:"60"},//left down 13
+    {top:"51", right:"32"},//left midle 14
+    {top:"42", right:"35"},//right midle 15
 ];
 
 var ballResourcePath = "img/balls/";
@@ -31,6 +32,8 @@ var ballImages = [
     "yellow_ball.png"
 ];
 
+var unavailableSymbols = ["#", '"', "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "_", "?", ".", ":", "-", "=", "%", "(", ")", "[", "]", "{", "}"];
+
 function login() {
     var isLoggedIn = getCookie("isLoggedIn") == null;
     if (isLoggedIn) {
@@ -43,23 +46,14 @@ function login() {
             var body = document.body;
             //login div parameters
             loginDiv.innerHTML = "Prosím zadajte svoje meno:";
-            loginDiv.style.textAlign = "center";
-            loginDiv.style.position = "absolute";
-            loginDiv.style.top = "40%";
-            loginDiv.style.left = "40%";
-            loginDiv.style.height = "100px";
-            loginDiv.style.width = "200px";
-            loginDiv.style.backgroundColor = "lightblue";
+            loginDiv.id = "loginDiv";
             //login input parameters
             loginInput.placeholder = "Vaše meno";
+            loginInput.id = "loginInput";
             loginInput.type = "text";
-            loginInput.id = "loginInput"
-            loginInput.style.margin = "8px";
-            loginInput.style.marginBottom = "0px";
             //login button config
             loginButton.innerHTML = "Potvrdiť";
-            loginButton.style.marginTop = "5px";
-            loginButton.style.marginLeft = "8px";
+            loginButton.id = "loginButton";
             loginButton.onclick = function() {
                 var name = document.getElementById("loginInput").value;
                 if (name != "") {
