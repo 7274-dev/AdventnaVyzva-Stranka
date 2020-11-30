808d40dcbfa14204f02e8a0ef51dc1981b97960d
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2020-11-30 17:34:21 +0100

    fix newlines

--- website/script.js
+++ website/script.js
@@ -272,6 +272,7 @@ function on_click(event) {
 
     http.onreadystatechange = function() {
         if (wasRequestSuccessful(this) && this.responseText != "") {
+            this.responseText.replace("\\n", "\n");
             description.innerHTML = JSON.parse(this.responseText).response;//should this be here? ".response"
             displayAditionalTagsFromServerResponse(JSON.parse(this.responseText).response);//should this be here? ".response"
         }
