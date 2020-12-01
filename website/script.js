18585b41c58bf8ecf3e371644b4a7ea91f5c8b9c
Daniel Svitan <70911709+Streamer272@users.noreply.github.com>
2020-12-01 10:31:02 +0100

    should be done now

--- website/script.js
+++ website/script.js
@@ -157,20 +157,11 @@ function getDate() {
 
 function easterEgg() {
     for(var i = 0; i < listOfNumbers.length(); i++){
-<<<<<<< HEAD
-        if (listOfNumbers[i] == 7 & listOfNumbers[i+1] == 2 & listOfNumbers[i+2] == 7 & listOfNumbers[i+3] == 4){
-            console.log("EasterEgg")
-        }; 
-    };
-
-}
-=======
         if (listOfNumbers[i] == 7 && listOfNumbers[i+1] == 2 && listOfNumbers[i+2] == 7 && listOfNumbers[i+3] == 4) {
             console.log("EasterEgg");
         } ;
     };
 };
->>>>>>> dda9b4fc782191624f5eff14627c851875b41d9d
 
 // Thank you Github gist
 // https://gist.github.com/comficker/871d378c535854c1c460f7867a191a5a#file-hex2rgb-js
