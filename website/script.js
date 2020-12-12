4510252a42f21f52482c9cf1c3cc2b354f136975
Daniel Svitan <70911709+Streamer272@users.noreply.github.com>
2020-12-12 09:23:17 +0100

    deleted all console.logs

--- website/script.js
+++ website/script.js
@@ -260,9 +260,7 @@ function getOpenedWindows(name) { //RETURN ALL DAYS DONE / HOMEWORK DONE
 
     var response;
     openedWindowsRequest.onreadystatechange = function() {
-        // console.log(this.responseText);
         if (wasRequestSuccessful(this)) {
-            // console.log(JSON.parse(this.responseText));
             response = JSON.parse(this.responseText).response;
         } else {
             response = null;
@@ -871,7 +869,6 @@ function setWindowData(name) {
     userExistsRequest.open("GET", url);
 
     userExistsRequest.onreadystatechange = function() {
-        // console.log(this.responseText);
         if (wasRequestSuccessful(this)) {
             createUser(name);
         } else {
@@ -888,7 +885,6 @@ function setWindowData(name) {
 function onloadBreakBall() {
     var openedBalls = getOpenedWindows(getCookie("loginName"));
     for (let ball in openedBalls) {
-        console.log("Breaking ball " + ball);
         breakeBall(ball);
     };
 };
@@ -990,9 +986,8 @@ function inTimeAllowed() {
 };
 
 function getOrientation() {
-    console.log(window.orientation % 90 == 0 && window.orientation != 0 ? "landscape" : "portrait");
     return window.orientation % 90 == 0 && window.orientation != 0 ? "landscape" : "portrait";
-}
+};
 
 function on_load() {
     login();
